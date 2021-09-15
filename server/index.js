d0e7a2a582d1b99bd32008a24da0c4cd4565527f
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-09-15 20:43:36 +0200

    First commit, setup and install dependencies

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
