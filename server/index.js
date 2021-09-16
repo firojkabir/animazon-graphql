e66d35ad104d560487ff4896f9e70441142f7f1e
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-09-16 15:59:27 +0200

    File organization, separate resolvers, schema from index file

--- server/index.js
+++ server/index.js
@@ -1,78 +1,21 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 
 const { mainCards, animals, categories } = require('./db');
 
-const typeDefs = gql`
-  type MainCard {
-      title: String!
-      image: String!
-  }
+const typeDefs = require('./schema')
 
-  type Animal {
-      id: ID!
-      image: String!
-      title: String!
-      rating: Float
-      price: String!
-      description: [String!]!
-      slug: String!
-      stock: Int!
-      onSale: Boolean
-      category: Category
-  }
+const Query = require('./resolvers/Query')
+const Category = require('./resolvers/Category')
+const Animal = require('./resolvers/Animal')
 
-  type Category {
-      id: ID!
-      image: String!
-      category: String!
-      slug: String!
-      animals: [Animal!]!
-  }
-
-  type Query {
-    mainCards: [MainCard]
-    animals: [Animal!]!
-    animal(slug: String!): Animal
-    categories: [Category!]!
-    category(slug: String!): Category
-  }
-`;
-
-const resolvers = {
-    Query: {
-        mainCards: () => mainCards,
-        animals: () => animals,
-        animal: (parent, args, ctx) => {
-            let animal = animals.find((animal) => {
-                return animal.slug === args.slug
-            })
-            return animal
-        },
-        categories: () => categories,
-        category: (parent, args, ctx) => {
-            let category = categories.find((category) => {
-                return category.slug === args.slug
-            });
-            return category;
-        }
-    },
-    Category: {
-        animals: (parent, args, ctx) => {
-            return animals.filter(animal => {
-                return animal.category === parent.id
-            })
-        }
-    },
-    Animal: {
-        category: (parent, args, ctx) => {
-            return categories.find((category) => {
-                return category.id === parent.category
-            })
-        }
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Query,
+        Category,
+        Animal
     }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
---
+++ server/resolvers/Animal.js
@@ -0,0 +1,9 @@
+const Animal = {
+    category: (parent, args, ctx) => {
+        return categories.find((category) => {
+            return category.id === parent.category
+        })
+    }
+}
+
+module.exports = Animal
