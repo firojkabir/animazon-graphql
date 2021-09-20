264e5efde32293a24d0944ad67186dbbd67a21ae
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-09-20 13:30:20 +0200

    Add an animal using mutation

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MainHero from "../components/MainHero/MainHero"
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
 import CardDisplay from "../components/CardDisplay/CardDisplay"
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, useMutation, gql } from '@apollo/client'
 
 const ANIMALS_QUERY = gql`
     {
@@ -17,9 +17,37 @@ const ANIMALS_QUERY = gql`
     }
 `
 
+const ADD_ANIMAL_MUTATION = gql`
+mutation(
+    $image: String!,
+    $category: String!,
+    $title: String!,
+    $stock: Int!,
+    $price: String!,
+    $description: [String!]!,
+    $rating: Float,
+    $slug: String!
+) {
+    addAnimal(
+        image: $image,
+        category: $category,
+        title: $title,
+        stock: $stock,
+        price: $price,
+        description: $description,
+        rating: $rating,
+        slug: $slug,
+    ) {
+        id
+    }
+}
+`
+
 function LandingPage() {
 
     const { loading, error, data } = useQuery(ANIMALS_QUERY)
+
+    const [addAnimal] = useMutation(ADD_ANIMAL_MUTATION)
 
     if (loading) return <div>Loading..</div>
 
@@ -30,6 +58,20 @@ function LandingPage() {
             <MainHero />
             <CategoryDisplay />
             <CardDisplay animals={data.animals} />
+            <button onClick={() => {
+                addAnimal({
+                    variables: {
+                        image: "cat",
+                        category: "1",
+                        title: "Mumu the meow",
+                        stock: 15,
+                        price: "4344",
+                        description: ["adffhhfgd"],
+                        rating: 4.5,
+                        slug: "cat"
+                    }
+                })
+            }}>Add an animal</button>
         </div>
     )
 }
